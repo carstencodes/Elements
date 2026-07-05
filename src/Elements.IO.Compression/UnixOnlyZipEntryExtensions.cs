f73afa8f5aae00106e522b6140ac54a08cb1c813
Carsten Igel <1760987+carstencodes@users.noreply.github.com>
2026-07-05 16:34:54 +0200

    feat: add Unix-only zip entry and file extensions for symbolic link support

---
+++ src/Elements.IO.Compression/UnixOnlyZipEntryExtensions.cs
@@ -0,0 +1,43 @@
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace HedgeCraft.Elements.IO.Compression;
+
+public static class UnixOnlyZipEntryExtensions
+{
+    private const int SymbolicLinkMarkerFlag = 0xA000;
+    private const int ByteShiftToShort = 16;
+    
+    public static bool IsSymbolicLink(this ZipArchiveEntry entry)
+    {
+        int attributes = entry.ExternalAttributes;
+        attributes >>= ByteShiftToShort;
+
+        return (attributes & SymbolicLinkMarkerFlag) == SymbolicLinkMarkerFlag;
+    }
+    
+    public static void MakeSymbolicLinkTo(this ZipArchiveEntry entry, FileInfo targetFile, Encoding? currentEncoding = null)
+    {
+        entry.MakeSymbolicLinkTo(targetFile.FullName, currentEncoding);
+    }
+    
+    public static void MakeSymbolicLinkTo(this ZipArchiveEntry entry, string targetFilePath, Encoding? currentEncoding = null)
+    {
+        UnixFileMode fileMode = UnixFileMode.UserRead | UnixFileMode.UserWrite | UnixFileMode.UserExecute
+                                | UnixFileMode.GroupRead | UnixFileMode.GroupWrite | UnixFileMode.GroupExecute
+                                | UnixFileMode.OtherRead | UnixFileMode.OtherWrite | UnixFileMode.OtherExecute;
+        int attributes = (int)fileMode;
+        attributes += SymbolicLinkMarkerFlag;
+        attributes <<= ByteShiftToShort;
+
+        entry.ExternalAttributes = attributes;
+        currentEncoding ??= Encoding.Default;
+
+        ReadOnlyMemory<byte> bytes = currentEncoding.GetBytes(targetFilePath);
+        using Stream stream = entry.Open();
+        stream.Write(bytes.Span);
+        stream.Close();
+    }
+}
