3861628d5d4b5eabadd39430718c17ee39270462
Carsten Igel <1760987+carstencodes@users.noreply.github.com>
2026-07-25 00:27:59 +0200

    qa. Added first dummy test

---
+++ tests/Elements.IO.Tests/PathInfoTests.cs
@@ -0,0 +1,12 @@
+namespace HedgeCraft.Elements.IO.Tests;
+
+using Xunit;
+using HedgeCraft.Elements.IO;
+
+public class PathInfoTests
+{
+    [Fact]
+    public void TestPathInfoCreate(){
+        Assert.True(1 > 0);
+    }
+}
