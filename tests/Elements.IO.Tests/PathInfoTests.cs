ebf2ff807fdb303261c7a3b7766333dd55c2e7a2
Carsten Igel <1760987+carstencodes@users.noreply.github.com>
2026-07-29 20:05:26 +0200

    qa: Migrate sources to TUnit

--- tests/Elements.IO.Tests/PathInfoTests.cs
+++ tests/Elements.IO.Tests/PathInfoTests.cs
@@ -1,12 +1,17 @@
 namespace HedgeCraft.Elements.IO.Tests;
 
-using Xunit;
+using System.Threading.Tasks;
+
+using TUnit;
+using TUnit.Assertions;
 using HedgeCraft.Elements.IO;
 
 public class PathInfoTests
 {
-    [Fact]
-    public void TestPathInfoCreate(){
-        Assert.True(1 > 0);
+    [Test]
+    public async Task TestPathInfoCreate()
+    {
+        int x = 1;
+        await Assert.That(x).IsGreaterThan(0);
     }
 }
