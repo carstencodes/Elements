2dfd8eb647090dbdbf92643c63714374b7423978
Carsten Igel <1760987+carstencodes@users.noreply.github.com>
2026-08-04 19:46:56 +0000

    chore(style): improve code safety and formatting in native bindings

--- src/Elements.IO.Posix.native-assets/src/effi.rs
+++ src/Elements.IO.Posix.native-assets/src/effi.rs
@@ -1,13 +1,13 @@
 pub enum CallResult {
     Ok = 0,
-    NullParameter = 1
+    NullParameter = 1,
 }
 
-impl Into<u16> for CallResult {
-    fn into(self) -> u16 {
-        match self {
+impl From<CallResult> for u16 {
+    fn from(value: CallResult) -> Self {
+        match value {
             CallResult::Ok => 0,
-            CallResult::NullParameter => 1
+            CallResult::NullParameter => 1,
         }
     }
 }
