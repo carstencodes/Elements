af23caa0a868014a3f8f052f8f26657acdfadd28
Carsten Igel <1760987+carstencodes@users.noreply.github.com>
2026-08-23 16:11:35 +0000

    dev: Add DevContainer to Specification to repository
    - Includes a feature to setup user id mapping correctly

---
+++ .devcontainer/features/src/user/install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+USER_NAME=${USERNAME:-"developer"}
+UID_TO_SET=${UID:-1000}
+GID_TO_SET=${GID:-1000}
+
+id ${UID_TO_SET} &>/dev/null && usermod -l ${USER_NAME} $(id -nu ${UID_TO_SET}) || adduser --uid ${UID_TO_SET} --gid ${GID_TO_SET} --disabled-password --gecos "" ${USER_NAME}
+
+id -G ${USER_NAME} | grep -q ${GID_TO_SET} || usermod -aG $(getent group ${GID_TO_SET} | cut -d: -f1) ${USER_NAME}
